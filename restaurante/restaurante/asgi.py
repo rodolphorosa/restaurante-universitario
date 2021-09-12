2509f406f000b3de5e99a6420f4ed4f434c35fc6
rodolphorosa <rodolphorosa05@gmail.com>
2021-09-12 16:53:31 -0300

    corrige nome do projeto: restaurate -> restaurante

--- restaurante/restaurante/asgi.py
+++ restaurante/restaurante/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for restaurate project.
+ASGI config for restaurante project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurante.settings')
 
 application = get_asgi_application()
